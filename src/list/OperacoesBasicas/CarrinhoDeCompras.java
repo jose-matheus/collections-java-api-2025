bc898f6f9fb48c3e0f920bf0e0be8df49fc5a8b9
José Matheus <132094255+jose-matheus@users.noreply.github.com>
2025-05-05 18:49:43 -0300

    exercicio de list, utilizando add, remove, e realizando operacoes de soma com for each

---
+++ src/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,57 @@
+package list.OperacoesBasicas;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        itemList.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome){
+        List<Item> itemRemover = new ArrayList<>();
+        for(Item o : itemList){
+            if(o.getNome().equalsIgnoreCase(nome)){
+                itemRemover.add(o);
+            }
+        }
+        itemList.removeAll(itemRemover);
+    }
+
+    public double calcularValorTotal(){
+
+        double valortotal = 0;
+        for(Item o : itemList){
+            valortotal += o.getPreco();
+        }
+        return valortotal;
+    }
+
+    public void exibirItens(){
+        System.out.println(itemList);
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("arroz", 14.09, 4);
+        carrinhoDeCompras.adicionarItem("feijao", 14.23, 4);
+        carrinhoDeCompras.adicionarItem("macarrao", 14.23, 4);
+        carrinhoDeCompras.adicionarItem("arroz", 14.55, 4);
+
+        carrinhoDeCompras.exibirItens();
+
+        carrinhoDeCompras.removerItem("arroz");
+
+        System.out.printf("Valor total do carrinho %.2f%n",carrinhoDeCompras.calcularValorTotal());
+
+        carrinhoDeCompras.exibirItens();
+    }
+
+}

ce44b6fef5bd25a0f6d1c9b1155c53818f0d8f14
José Matheus <132094255+jose-matheus@users.noreply.github.com>
2025-05-06 20:56:31 -0300

    exercicio de fixação da utilização de listas e suas operações mais basicas

---
+++ src/list/OperacoesBasicas/CatalogoLivros.java
@@ -0,0 +1,56 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+
+    private List<Livro> livros;
+
+    public CatalogoLivros() {
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livros.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public void removerLivroTitulo(String titulo){
+        List<Livro> livrosRemover = new ArrayList<>();
+        for(Livro l : livros){
+            if(l.getTitulo().equalsIgnoreCase(titulo)){
+                livrosRemover.add(l);
+            }
+        }
+        livros.removeAll(livrosRemover);
+    }
+
+    public List<Livro> pesquisarAutor(String autor){
+        List<Livro> livrosAutor = new ArrayList<>();
+        for(Livro l : livros){
+            if(l.getAutor().equalsIgnoreCase(autor)){
+                livrosAutor.add(l);
+            }
+        }
+        return livrosAutor;
+    }
+
+    public List<Livro> pesquisarIntervaloAnos(int dInicial, int dFinal){
+        List<Livro> livrosNoIntervalo = new ArrayList<>();
+        for(Livro l : livros){
+            if(l.getAnoDePublicacao() >= dInicial && l.getAnoDePublicacao() <= dFinal){
+                livrosNoIntervalo.add(l);
+            }
+        }
+        return livrosNoIntervalo;
+    }
+
+    public void exibirLivros(){
+        if(livros.isEmpty()){
+            System.out.println("Você não tem livros!");
+        }else {
+            System.out.println(livros);
+        }
+    }
+
+}

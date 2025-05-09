bb93ff1e90357484da7bc8c7ccaf788b847fb017
José Matheus <132094255+jose-matheus@users.noreply.github.com>
2025-05-09 15:11:07 -0300

    ordenação de sets utilizando comparator e comparable
    Signed-off-by: José Matheus <132094255+jose-matheus@users.noreply.github.com>

---
+++ src/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,28 @@
+package set.Ordenacao;
+
+import java.util.*;
+
+public class CadastroProdutos {
+
+    //atributo
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long codigo, String nome, double preco, int quantidade){
+        produtoSet.add(new Produto(codigo, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome(){
+        return new TreeSet<>(produtoSet);
+    }
+
+    public Set<Produto> exibirPorPreco(){
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+}
+
